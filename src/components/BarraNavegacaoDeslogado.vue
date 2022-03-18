c17e98911a34f76cb36430b1fadabb482e0d119e
AckermanFernando <fernandoloiola03@gmail.com>
2022-03-18 17:33:37 -0300

    barra de navegação para  usuários não logados

---
+++ src/components/BarraNavegacaoDeslogado.vue
@@ -0,0 +1,11 @@
+<template>
+  <ul class="navbar-nav mr-auto">
+    <li class="nav-item">
+      <router-link to="/novousuario" class="nav-link"> Cadatrar </router-link>
+    </li>
+    <li class="nav-item">
+      <router-link to="/login" class="nav-link"> Login </router-link>
+    </li>
+  </ul>
+</template>
+
